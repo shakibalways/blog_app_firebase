2cf22a2832d4992c3862a52c005c89d5637827ef
S <shakibmohammad18@gmail.com>
2024-07-28 02:27:30 +0600

    User Successfully Register And Create Done

--- lib/auth/sign_in/sign_in.dart
+++ lib/auth/sign_in/sign_in.dart
@@ -1,5 +1,9 @@
 import 'package:blog_app_firebase/global_widget/round_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -9,106 +13,127 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
-  bool showSpinner = false;
 
-  TextEditingController userController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
-
   final _formKey = GlobalKey<FormState>();
-  String user = "";
-  String email = "";
-  String pass = "";
+  String email = "", password = "";
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        leading: const Icon(
-          Icons.arrow_back_rounded,
-          color: Colors.white,
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          leading: const Icon(
+            Icons.arrow_back_rounded,
+            color: Colors.white,
+          ),
+          title: const Text(
+            "Create Account",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
         ),
-        title: const Text(
-          "Create Account",
-          style: TextStyle(color: Colors.white),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                "Register",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            // hintText: "User Name",
+                            labelText: "Email",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                        onChanged: (String value) {
+                          email = value;
+                        },
+                        validator: (value) {
+                          return value!.isEmpty ? "Enter Your Email" : null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        controller: passController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            // hintText: "User Name",
+                            labelText: "Password",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        validator: (value) {
+                          return value!.isEmpty ? "Enter Your Password" : null;
+                        },
+                      ),
+                    ],
+                  )),
+              RoundButton(
+                  title: "Register",
+                  onPress: () async {
+                    if (_formKey.currentState!.validate()) {
+                      setState(() {
+                        showSpinner=true;
+                      });
+                      try{
+
+                        final user =
+                        await _auth.createUserWithEmailAndPassword(
+                          email: email.toString().trim(),
+                          password: password.toString().trim(),
+                        );
+                        if(user != null){
+                          print("Success");
+                          toastMessages("User Successfully Created");
+                          setState(() {
+                            showSpinner=false;
+                          });
+
+                        }
+                      }catch(e){
+                        print(e.toString());
+                        toastMessages(e.toString());
+                        setState(() {
+                          showSpinner=false;
+                        });
+                      }
+                    }
+                  },
+                  color: Colors.deepPurple)
+            ],
+          ),
         ),
-        centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text(
-              "Register",
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
-            Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: userController,
-                      decoration: InputDecoration(
-                          // hintText: "User Name",
-                          labelText: "User Name",
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      onChanged: (String value) {
-                        user = value;
-                      },
-                      validator: (value) {
-                        return value!.isEmpty ? "Enter User Name" : null;
-                      },
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    TextFormField(
-                      controller: emailController,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                          // hintText: "User Name",
-                          labelText: "Email",
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      onChanged: (String value) {
-                        email = value;
-                      },
-                      validator: (value) {
-                        return value!.isEmpty ? "Enter Your Email" : null;
-                      },
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    TextFormField(
-                      controller: passController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          // hintText: "User Name",
-                          labelText: "Password",
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      onChanged: (value) {
-                        pass = value;
-                      },
-                      validator: (value) {
-                        return value!.isEmpty ? "Enter Your Password" : null;
-                      },
-                    ),
-                  ],
-                )),
-            RoundButton(
-                title: "Register",
-                onPress: () async {
-                  if (_formKey.currentState!.validate()) {}
-                },
-                color: Colors.deepPurple)
-          ],
-        ),
-      ),
+    );
+  }
+  void toastMessages(messages){
+    Fluttertoast.showToast(
+        msg: messages,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.SNACKBAR,
+        timeInSecForIosWeb: 3,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
     );
   }
 }
